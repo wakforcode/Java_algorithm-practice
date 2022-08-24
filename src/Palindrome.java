e890f8f0819af6296b92d9b7744baad578f0b7b8
wangzhikai <wangzhikai@yit.com>
2022-08-24 18:18:53 +0800

    8.24

---
+++ src/Palindrome.java
@@ -0,0 +1,51 @@
+/**
+ * @author wzk
+ * @description
+ * @Date 2022/8/24 14:41
+ */
+public class Palindrome {
+
+    /**
+     * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+     * <p>
+     * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+     * <p>
+     * 例如，121 是回文，而 123 不是。
+     */
+    public static boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        String s = Integer.toString(x);
+        int[] arr = new int[s.length()];
+        int a = x;
+        int b = 0;
+        int i = 0;
+        do {
+            b = a % 10;
+            arr[i] = b;
+            a = a / 10;
+            i++;
+        } while (a != 0);
+
+
+        int index = arr.length - 1;
+        for (int j = 0; j < arr.length; j++) {
+
+            if (j > index / 2) {
+                break;
+            }
+            if (arr[j] != arr[index - j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome(121121));
+    }
+
+
+}
---
+++ src/longestCommonPrefix.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wzk
+ * @description
+ * @Date 2022/8/24 15:47
+ */
+public class longestCommonPrefix {
+
+    /**
+     * 编写一个函数来查找字符串数组中的最长公共前缀。
+     * <p>
+     * 如果不存在公共前缀，返回空字符串 ""。
+     * 示例 1：
+     * <p>
+     * 输入：strs = ["flower","flow","flight"]
+     * 输出："fl"
+     * 示例 2：
+     * <p>
+     * 输入：strs = ["dog","racecar","car"]
+     * 输出：""
+     * 解释：输入不存在公共前缀。
+     */
+
+    public String longestCommonPrefix(String[] strs) {
+
+        if (strs.length == 0) {
+            return "";
+        }
+        String str = strs[0];
+        int length = str.length();
+        int index = 0;
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].length() <= length) {
+                length = strs[i].length();
+                index = i;
+            }
+        }
+        String shortStr = strs[index];
+        StringBuilder s = new StringBuilder();
+        char[] shortChars = shortStr.toCharArray();
+        for (int j = 0; j < shortChars.length; j++) {
+
+            for (int i = 0; i < strs.length; i++) {
+                String str1 = strs[i];
+                char[] chars = str1.toCharArray();
+                if (shortChars[j] != chars[j]) {
+                    return s.toString();
+                }
+                if (shortChars[j] == chars[j] && i == strs.length - 1) {
+                    s.append( shortChars[j]);
+                }
+            }
+        }
+        if (s.length()>0) {
+            return s.toString();
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        longestCommonPrefix prefix = new longestCommonPrefix();
+        String[] strings = new String[]{"cir","car"};
+        String s = prefix.longestCommonPrefix(strings);
+        System.out.println(s);
+    }
+
+
+}
