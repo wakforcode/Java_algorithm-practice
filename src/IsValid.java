b9a363c583324116c8f9874d10c23e13cb647927
wangzhikai <wangzhikai@yit.com>
2022-08-25 18:34:58 +0800

    8.25 写的不爽

--- src/IsValid.java
+++ src/IsValid.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 /**
  * @author wzk
  * @description
@@ -14,6 +18,65 @@ public class IsValid {
      * 左括号必须以正确的顺序闭合。
      *
      */
+
+    /**
+     * 示例 1：
+     * <p>
+     * 输入：s = "()"
+     * 输出：true
+     * 示例2：
+     * <p>
+     * 输入：s = "()[]{}"
+     * 输出：true
+     * 示例3：
+     * <p>
+     * 输入：s = "(]"
+     * 输出：false：
+     * <p>
+     * 输入：s = "([)]"
+     * 输出：false
+     * 示例5：
+     * <p>
+     * 输入：s = "{[]}"
+     * 输出：true
+     *
+     * @param s
+     * @return
+     */
+
+    Map<Character, Character> symbolValues = new HashMap<Character, Character>() {{
+        put('(', ')');
+        put('[', ']');
+        put('{', '}');
+    }};
+
+    public boolean isValid(String s) {
+        if (s.length() == 0 || s.length() % 2 != 0) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (symbolValues.get(stack.peek()) == c) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        IsValid isValid = new IsValid();
+        isValid.isValid("()");
+    }
 
 
 }
---
+++ src/RemoveElement.java
@@ -0,0 +1,84 @@
+/**
+ * @author wzk
+ * @description
+ * @Date 2022/8/25 14:32
+ */
+public class RemoveElement {
+
+    // TODO: 2022/8/25 好好想想 没做完 ！
+
+    /**
+     * 给你一个数组 nums和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+     * <p>
+     * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+     * <p>
+     * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：nums = [3,2,2,3], val = 3
+     * 输出：2, nums = [2,2]
+     * 解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+     * 示例 2：
+     * <p>
+     * 输入：nums = [0,1,2,2,3,0,4,2], val = 2
+     * 输出：5, nums = [0,1,4,0,3]
+     * 解释：函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。注意这五个元素可为任意顺序。你不需要考虑数组中超出新长度后面的元素。
+     * <p>
+     * [0,1,2,2,3,0,4,2]
+     * <p>
+     * [0,1,4,0,3]
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+
+            // 说明完成了替换 算长度
+            if(left==right||left==nums.length-1){
+                if(nums[left]==val){
+                    return left;
+                }else {
+                    return left-1;
+                }
+            }
+
+            if (nums[left] == val && nums[right] != val) {
+                nums[left] = nums[right];
+                nums[right] = val;
+                left++;
+                right--;
+            }else {
+                if(nums[left] != val){
+                    left++;
+                }
+                if(nums[right] == val){
+
+                    right--;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        RemoveElement removeElement = new RemoveElement();
+
+        int[] ints = {3,2,2,3};
+        int i = removeElement.removeElement(ints, 3);
+
+        for (int j = 0; j < i; j++) {
+
+            System.out.println(ints[j]);
+        }
+
+    }
+}
